6be08a1516dd0ae660e39eb4cc9268aee847aeaa
Chris Palmeri <chrispalmeri@gmail.com>
2017-09-05 21:54:59 -0500

    Update deploy script

--- deploy.php
+++ deploy.php
@@ -1,3 +1,12 @@
 <?php
-require '../../deploy.php';
+
+$input = file_get_contents('php://input');
+$hash = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+$secret = $_ENV['DEPLOY_SECRET'];
+
+if('sha1=' . hash_hmac('sha1', $input, $secret, false) === $hash) {
+    shell_exec('cd /var/www/reallysketch.com/public_html');
+    echo shell_exec('git pull');
+}
+
 ?>
