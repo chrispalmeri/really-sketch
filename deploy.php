9c4d5902014762e60894f60c6c2e05dd65be19f5
Chris Palmeri <chrispalmeri@gmail.com>
2017-09-05 22:21:37 -0500

    Fix deploy script

--- deploy.php
+++ deploy.php
@@ -2,7 +2,7 @@
 
 $input = file_get_contents('php://input');
 $hash = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-$secret = $_ENV['DEPLOY_SECRET'];
+$secret = $_SERVER['DEPLOY_SECRET'];
 
 if('sha1=' . hash_hmac('sha1', $input, $secret, false) === $hash) {
     shell_exec('cd /var/www/reallysketch.com/public_html');
