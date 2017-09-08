9ff857bfb359a70298f27b797d6399f409763cc1
Chris Palmeri <chrispalmeri@gmail.com>
2017-09-07 21:47:18 -0500

    Limit deploy to master

--- deploy.php
+++ deploy.php
@@ -5,8 +5,13 @@ $hash = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 $secret = $_SERVER['DEPLOY_SECRET'];
 
 if('sha1=' . hash_hmac('sha1', $input, $secret, false) === $hash) {
-    shell_exec('cd /var/www/reallysketch.com/public_html');
-    echo shell_exec('git pull');
+    $data = json_decode($input);
+    if($data->ref === 'refs/heads/master') {
+        shell_exec('cd /var/www/reallysketch.com/public_html');
+        echo shell_exec('git pull');
+    } else {
+        echo 'Push was not to master';
+    }
 }
 
 ?>
